df58b37630cd8c31cf0e28917874be8c758a4805
aheenxD <aheendutta3@gmail.com>
2024-06-21 23:43:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage1() {
+    document.getElementById('message').innerText = "Happy 8 months anniversary my love !! I love you so muchh paakhii 💞";
+}
+
+function showMessage2() {
+    document.getElementById('message').innerText = "i love you so so so so so much , more than any words can possibly explain my love 💗, thank youu for being such a cutuu , i dont know what i'd be doing in this world without youu , you make me complete , you are literally my significant other 😍, lets marry quick bruh i cannot wait❣️ , i wanna live with you forever  , you are the greatest person i've every met , the most amazing , loyal , cute, goregous , kind , fun girl i've known , i didnt even realize , its been 8 months and look at us , you make so comfy i've never been so comfortable with anybody 😭 , thank you for making me feel worth something :)💖 , you're the best girlfriend🥰 !!  ";
+}
+
+function showMessage3() {
+    document.getElementById('message').innerText = "Yayyy my cutiee piee , my angel , You are Perfect , I love you 💟💌💘💞😻💓💗💖❣️💟💝💘🩷🧡💛💚💙🩵💜🤎🖤🩶🤍❤️‍🔥💕";
+}
